c1952a53bbeb12e479a7239f737f07513ce3c4e9
andrew- <cubedoodldev@gmail.com>
2020-07-07 13:47:22 -0400

    Greatly improve customizability, add double buffering, and update README with relevant configuration information

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # faceid\_lock.py
 
-A facial recognition lockscreen for X11.  Not recommended for use in security-sensitive situations, but still a fun tinker toy.  This is the Python-only version of [this repository](https://github.com/Cubified/faceid_lock), bringing increased customizability and user-friendliness.
+A facial recognition lockscreen for X11.  Not recommended for use in security-sensitive situations, but still a fun tinker toy.  This is the Python-only version of [this repository](https://github.com/Cubified/faceid_lock), bringing increased customizability, speed, and user-friendliness.
 
 ## Screenshots
 
@@ -24,11 +24,42 @@ Assuming all dependencies have been installed properly (which unfortunately cann
 
      $ ./faceid_lock.py
 
-To ensure that everything has installed successfully.  It should return an error while also creating the `faceid_lock` configuration directory in `~/.local/share` where an image named `user_image` must be present.  This image can be in any common file format (JPG, PNG, etc.).
+To ensure that everything has installed successfully.  It should return an error (as it will have been unable to find the `user_image` file required for facial recognition) while also creating the `faceid_lock` configuration directory in `~/.local/share`.  To configure and run properly, see below.
 
 ## Configuration
 
-Upon first run, `faceid_lock.py` should create a `conf.ini` file in `~/.local/share/faceid_lock` which contains all available configuration options.  Specifically, these are:
+Upon first run, `faceid_lock.py` should create a `conf.ini` file in `~/.local/share/faceid_lock` which contains a barebones example of a configuration file.  `faceid_lock.py` can be configured further as follows:
+
+```ini
+[example_rectangle]                  ; Section name, can be anything but must be unique
+type = rectangle                     ; Required
+x = 10                               ; Required
+y = 10                               ; Required
+w = 100                              ; Required
+h = 50                               ; Required
+color = [rgb:ff/12/34 | red]         ; X11-style color triple or color name
+image = /path/to/image_file.png      ; A path to an image in any common format
+fit_image = true                     ; Resize image to fit width and height specified earlier (does not respect aspect ratio)
+
+[example_text]                       ; Section name, can be anything but must be unique
+type = text                          ; Required
+x = 20                               ; Required
+y = 20                               ; Required
+font = -*-*-*-*-*-*-12-*-*-*-*-*-*-* ; X11-style font selector, use xlsfonts or xfontsel to view choices
+                                     ; One of the following three is required
+text = hello world                   ; String literal to be printed
+text = STATUS                        ; User-friendly status text (e.g. "Setting up...," "Checking...," "Face not recognized.")
+command = date                       ; Command to be executed on redraw, output is printed
+
+[X]                                  ; Section entitled X affects entire screen, all keys are optional
+color = [rgb:33/33/33 | gray]        ; X11 color triple or color name
+background = /path/to/background.png ; An image file to be placed in the background, can be in any common format
+fit_image = true                     ; Whether the background image should be scaled to fit the entire screen
+
+[facial_rec]                         ; All keys in this section are required
+user_image = /path/to/user_image     ; Path to an image of the user's face, can be in any common format
+webcam = [kinect | cv]               ; Webcam backend to use, kinect will use freenect while cv will use OpenCV
+```
 
 - `[X]` Section:
   - `background`: A path to a background image
@@ -45,3 +76,9 @@ Upon first run, `faceid_lock.py` should create a `conf.ini` file in `~/.local/sh
 Although there exists no automatic way to install this script, this can be achieved through:
 
      $ cp faceid_lock.py ~/.local/bin
+
+## To-Do
+
+- Cache background images to avoid slight lag on redraw
+- Transparency
+- More input backends than solely face?
--- faceid_lock.py
+++ faceid_lock.py
@@ -4,11 +4,11 @@
 # faceid_lock.py: a facial
 # recognition lock for X11
 #
-
 import os
 import io
 import argparse
 import configparser
+import subprocess
 
 parser = argparse.ArgumentParser(description='Lock the current X display.')
 parser.add_argument('--config', metavar='conf.ini', type=str, help='a config file to read (default is ~/.local/share/faceid_lock/conf.ini)')
@@ -23,15 +23,7 @@ default_configdir = os.path.expanduser('~/.local/share/faceid_lock')
 default_configfile = default_configdir+'/conf.ini'
 config = configparser.ConfigParser()
 config['X'] = {
-    'background': default_configdir+'/background.png',
-    'box_x': '20',
-    'box_y': '1010',
-    'box_w': '150',
-    'box_h': '50',
-    'text_x': '30',
-    'text_y': '1038',
-    'text_color': 'black',
-    'box_color': 'white'
+    'color': 'rgb:00/00/00'
 }
 config['facial_rec'] = {
     'user_image': default_configdir+'/user_image',
@@ -55,24 +47,151 @@ if not args.config == None:
 #
 # (Catches display error
 #  before loading face_rec)
+#
 from Xlib import X, display, Xcursorfont
 from PIL import Image
+# import numpy as np
 
-def disp(dpy, win, gc, fg, bg, msg):
+status = 'Setting up...'
+# cam_data = np.asarray([0])
+
+def x_draw_rect(dpy, win, gc, dict, verify_params=True):
+    if verify_params:
+        if not ('x' in dict and
+                'y' in dict and
+                'w' in dict and
+                'h' in dict):
+            print('Warning: rectangle definition must contain at least x, y, w, and h values.')
+            return
+    else:
+        dict['x'] = '0'
+        dict['y'] = '0'
+        dict['w'] = str(dpy.screen().width_in_pixels)
+        dict['h'] = str(dpy.screen().height_in_pixels)
+        if 'background' in dict:
+            dict['image'] = dict['background']
+
+    if 'color' in dict:
+        cmap = dpy.screen().default_colormap
+        color = cmap.alloc_named_color(dict['color'])
+        if not color == None:
+            gc.change(
+                foreground=color.pixel
+            )
+            win.fill_rectangle(
+                gc,
+                int(dict['x']), int(dict['y']),
+                int(dict['w']), int(dict['h']),
+            )
+        else:
+            print('Warning: unrecognized color "'+dict['color']+'" in rectangle definition.')
+    
+    if 'image' in dict:
+        try:
+            bgd_img = Image.open(os.path.expanduser(dict['image']))
+        except FileNotFoundError:
+            print('Warning: image "'+os.path.expanduser(dict['image'])+'" in rectangle definition not found.')
+        else:
+            bgd_img = bgd_img.convert(mode="RGB")
+            if('fit_image' in dict and
+               dict['fit_image'] == 'true'):
+                bgd_img = bgd_img.resize((int(dict['w']), int(dict['h'])))
+            win.put_pil_image(gc, 0, 0, bgd_img)
+    
+def x_draw_text(dpy, win, gc, dict):
+    if not ('x' in dict and
+            'y' in dict and
+           ('text' in dict or
+            'command' in dict)):
+        print('Warning: text definition must contain at least x and y values, and one of either text or command values.')
+        return
+
     gc.change(
-        foreground=bg
+        foreground=dpy.screen().black_pixel
     )
-    win.fill_rectangle(
+
+    if 'font' in dict:
+        font = dpy.open_font(dict['font'])
+        if not font == None:
+            gc.change(
+                font=font
+            )
+        else:
+            print('Warning: font "'+dict['font']+'" not found.')
+
+    if 'color' in dict:
+        cmap = dpy.screen().default_colormap
+        color = cmap.alloc_named_color(dict['color'])
+        if not color == None:
+            gc.change(
+                foreground=color.pixel
+            )
+            if 'command' in dict:
+                string = subprocess.check_output(['sh', '-c', dict['command']])
+            else:
+                if dict['text'] == 'STATUS':
+                    global status
+                    string = status
+                else:
+                    string = dict['text']
+
+            win.draw_text(
+                gc,
+                int(dict['x']), int(dict['y']),
+                string
+            )
+        else:
+            print('Warning: unrecognized color "'+dict['color']+'" in text definition.')
+
+"""
+This is quite lag-heavy and largely useless, as the image is only painted on mouse click.
+
+All other commented lines allow this to work, however until I go about optimizing this
+I will leave it commented out
+
+def x_draw_cam(dpy, win, gc, dict):
+    if not ('x' in dict and
+            'y' in dict and
+            'w' in dict and
+            'h' in dict):
+        print('Warning: webcam definition must contain at least x, y, w, and h values.')
+        return
+    
+    global cam_data
+
+    try:
+        win.put_pil_image(
+            gc,
+            int(dict['x']), int(dict['y']),
+            Image.fromarray(cam_data)
+                 .resize(
+                    (int(dict['w']), int(dict['h']))
+                 )
+        )
+    except TypeError:
+        pass
+"""
+
+def x_draw(dpy, win, gc, buf, buf_gc):
+    if 'X' in config:
+        x_draw_rect(dpy, buf, buf_gc, config['X'], verify_params=False)
+    for key in config:
+        if 'type' in config[key]:
+            if config[key]['type'] == 'rectangle':
+                x_draw_rect(dpy, buf, buf_gc, config[key])
+            elif config[key]['type'] == 'text':
+                x_draw_text(dpy, buf, buf_gc, config[key])
+#            elif config[key]['type'] == 'webcam':
+#                x_draw_cam(dpy, buf, buf_gc, config[key])
+    win.copy_area(
         gc,
-        int(config['X']['box_x']), int(config['X']['box_y']),
-        int(config['X']['box_w']), int(config['X']['box_h'])
+        buf,
+        0, 0,
+        dpy.screen().width_in_pixels, dpy.screen().height_in_pixels,
+        0, 0
     )
-    gc.change(
-        foreground=fg
-    )
-    win.draw_text(gc, int(config['X']['text_x']), int(config['X']['text_y']), msg)
 
-def init():
+def x_init():
     dpy = display.Display()
     scr = dpy.screen()
     win = scr.root.create_window(
@@ -87,13 +206,12 @@ def init():
         colormap = X.CopyFromParent
     )
     gc = win.create_gc()
-    try:
-        bgd_img = Image.open(os.path.expanduser(config['X']['background']))
-    except FileNotFoundError:
-        print('Warning: background image "'+os.path.expanduser(config['X']['background'])+'" not found.')
-        bgd_img = Image.new("RGB", (scr.width_in_pixels, scr.height_in_pixels))
-    else:
-        bgd_img = bgd_img.convert(mode="RGB")
+    buf = win.create_pixmap(
+        scr.width_in_pixels,
+        scr.height_in_pixels,
+        scr.root_depth
+    )
+    buf_gc = buf.create_gc()
 
     if not args.test:
         scr.root.grab_pointer(
@@ -112,48 +230,43 @@ def init():
             X.CurrentTime
         )
 
-    cmap = scr.default_colormap
-    fg = cmap.alloc_named_color(config['X']['text_color'])
-    bg = cmap.alloc_named_color(config['X']['box_color'])
-    if fg == None or bg == None:
-        print('Warning: unrecognized text and/or box colors "'+config['X']['text_color']+'" and "'+config['X']['box_color']+'".')
-        fg = scr.black_pixel
-        bg = scr.white_pixel
-    else:
-        fg = fg.pixel
-        bg = bg.pixel
-
-    win.set_wm_name('faceid_lock')
     win.map()
-    win.put_pil_image(gc, 0, 0, bgd_img)
-    disp(dpy, win, gc, fg, bg, 'Setting up...')
+    x_draw(dpy, win, gc, buf, buf_gc)
     dpy.sync()
 
     return {
         'dpy': dpy,
         'win': win,
         'gc':  gc,
-        'fg':  fg,
-        'bg':  bg
+        'buf': buf,
+        'buf_gc': buf_gc
     }
 
-def loop(dpy, win, gc, fg, bg):
+def x_loop(dpy, win, gc, buf, buf_gc):
     evt = dpy.next_event()
     if evt.type == X.ButtonPress and evt.detail == 1:
-        disp(dpy, win, gc, fg, bg, 'Checking...')
+        global status
+        status = 'Checking...'
+        x_draw(dpy, win, gc, buf, buf_gc)
         dpy.sync()
+
         out = check_face()
-        if out == 'success':
+        if out:
             dpy.ungrab_pointer(0)
             dpy.ungrab_keyboard(0)
             win.unmap()
+            buf.free()
+            win.destroy()
             dpy.close()
             exit(0)
         else:
-            disp(dpy, win, gc, fg, bg, out)
-    loop(dpy, win, gc, fg, bg)
+            x_draw(dpy, win, gc, buf, buf_gc)
+    x_loop(dpy, win, gc, buf, buf_gc)
 
-x_vars = init()
+##############################
+# Entry point
+#
+x_vars = x_init()
 
 ##############################
 # Facial rec (heavy includes,
@@ -173,27 +286,37 @@ except FileNotFoundError:
     exit(1)
 
 def check_face():
+    global status
+#    global cam_data
+
     if config['facial_rec']['webcam'] == 'kinect':
         img = freenect.sync_get_video()[0]
     else:
         cap = cv2.VideoCapture(0)
         ret, img = cap.read()
         cap.release()
+
     if img.size == 0:
-        return 'Webcam error.'
+        status = 'Webcam error.'
+        return 0
+
+#    cam_data = img
+
     all_enc = face_recognition.face_encodings(img)
     if(len(all_enc) > 0):
         new = all_enc[0]
         sim = face_recognition.compare_faces([enc],new)
-        if True in sim:
-            return 'success'
-        else:
-            return 'Face not recognized.'
+        if not True in sim:
+            status = 'Face not recognized.'
+            return 0
+        return 1
     else:
-        return 'No faces detected.'
+        status = 'No faces detected.'
+        return 0
 
 ##############################
 # Event listener loop
 #
-disp(x_vars['dpy'], x_vars['win'], x_vars['gc'], x_vars['fg'], x_vars['bg'], 'Click to unlock.')
-loop(**x_vars)
+status = 'Click to unlock.'
+x_draw(**x_vars)
+x_loop(**x_vars)
